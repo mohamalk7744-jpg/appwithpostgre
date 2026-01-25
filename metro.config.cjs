b1d6d33c803ab84f4eadd31241f532bc69321300
mohamalk7744-jpg <mohamalk7744@gmail.com>
2026-01-25 22:55:25 +0300

    Update metro.config.cjs

--- metro.config.cjs
+++ metro.config.cjs
@@ -4,4 +4,16 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+config.server = {
+  ...config.server,
+  enhanceMiddleware: (middleware) => {
+    return (req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      return middleware(req, res, next);
+    };
+  },
+};
+
 module.exports = config;
